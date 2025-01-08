bd030984459f820816d0f38c2a094e0fcd3ef2e7
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-01-08 12:23:01 +0700

    source version 3: create 1 file

---
+++ lib/contact_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AAAAAA extends StatefulWidget {
+  const AAAAAA({super.key});
+
+  @override
+  State<AAAAAA> createState() => _AAAAAAState();
+}
+
+class _AAAAAAState extends State<AAAAAA> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
