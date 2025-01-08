ef5589636487c6e6aa84e61ca218bcd49213a687
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-01-08 12:11:41 +0700

    source version 2: create 2 file na ja ู ^_^

---
+++ lib/about_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class YYYYU extends StatefulWidget {
+  const YYYYU({super.key});
+
+  @override
+  State<YYYYU> createState() => _YYYYUState();
+}
+
+class _YYYYUState extends State<YYYYU> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/home_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class XXXXXX extends StatefulWidget {
+  const XXXXXX({super.key});
+
+  @override
+  State<XXXXXX> createState() => _XXXXXXState();
+}
+
+class _XXXXXXState extends State<XXXXXX> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
